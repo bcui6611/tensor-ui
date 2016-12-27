c20ec63804044e4c4a0043c4bb4f332f06488bc5
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-27 09:44:30 +0530

    Add sample data for permission table

---
+++ src/app/settings/teams/teamPermissions/table-permissions.ts
@@ -0,0 +1,504 @@
+export const UserPermissionsData: Array<any> = [
+    {
+        'name': 'Victoria Cantrell',
+        'type': 'Integer Corporation',
+        'role': 'Croatia',
+    },
+    {
+        'name': 'Pearl Crosby',
+        'type': 'In PC',
+        'role': 'Cambodia',
+    },
+    {
+        'name': 'Colette Foley',
+        'type': 'Lorem Inc.',
+        'role': 'Korea, North',
+    },
+    {
+        'name': 'Anastasia Shaffer',
+        'type': 'Dolor Nulla Semper LLC',
+        'role': 'Suriname',
+    },
+    {
+        'name': 'Gabriel Castro',
+        'type': 'Sed Limited',
+        'role': 'Bahrain',
+    },
+    {
+        'name': 'Cherokee Ware',
+        'type': 'Tincidunt LLC',
+        'role': 'United Kingdom (Great Britain)',
+    },
+    {
+        'name': 'Barry Moss',
+        'type': 'Sociis Industries',
+        'role': 'Western Sahara',
+    },
+    {
+        'name': 'Maryam Tucker',
+        'type': 'Elit Pede Malesuada Inc.',
+        'role': 'Brazil',
+    },
+    {
+
+        'name': 'Constance Clayton',
+        'type': 'Auctor Velit Aliquam LLP',
+        'role': 'United Arab Emirates',
+    },
+    {
+        'name': 'Rogan Tucker',
+        'type': 'Arcu Vestibulum Ante Associates',
+        'role': 'Jersey',
+    },
+    {
+        'name': 'Emery Mcdowell',
+        'type': 'Gravida Company',
+        'role': 'New Zealand',
+    },
+    {
+        'name': 'Yael Greer',
+        'type': 'Orci Limited',
+        'role': 'Madagascar',
+    },
+    {
+        'name': 'Jared Burgess',
+        'type': 'Auctor Incorporated',
+        'role': 'Burundi',
+    },
+    {
+        'name': 'Sharon Campbell',
+        'type': 'Elit Curabitur Sed Consulting',
+        'role': 'Comoros',
+    },
+    {
+        'name': 'Yeo Church',
+        'type': 'Donec Vitae Erat PC',
+        'role': 'Saudi Arabia',
+    },
+    {
+        'name': 'Kylie Barlow',
+        'type': 'Fermentum Risus Corporation',
+        'role': 'Papua New Guinea',
+    },
+    {
+        'name': 'Nell Leonard',
+        'type': 'Vestibulum Consulting',
+        'role': 'Saudi Arabia',
+    },
+    {
+        'name': 'Brandon Fleming',
+        'type': 'Donec Egestas Associates',
+        'role': 'Poland',
+    },
+    {
+        'name': 'Inga Pena',
+        'type': 'Et Magnis Dis Limited',
+        'role': 'Belgium',
+    },
+    {
+        'name': 'Arden Russo',
+        'type': 'Est Tempor Bibendum Corp.',
+        'role': 'Dominican Republic',
+    },
+    {
+        'name': 'Liberty Gallegos',
+        'type': 'Nec Diam LLC',
+        'role': 'Ghana',
+    },
+    {
+        'name': 'Dennis York',
+        'type': 'Nullam Suscipit Foundation',
+        'role': 'Namibia',
+    },
+    {
+        'name': 'Petra Chandler',
+        'type': 'Pede Nonummy Inc.',
+        'role': 'Namibia',
+    },
+    {
+        'name': 'Aurelia Marshall',
+        'type': 'Donec Consulting',
+        'role': 'Nicaragua',
+    },
+    {
+        'name': 'Rose Carter',
+        'type': 'Enim Consequat Purus Industries',
+        'role': 'Morocco',
+    },
+    {
+        'name': 'Denton Atkins',
+        'type': 'Non Vestibulum PC',
+        'role': 'Mali',
+    },
+    {
+        'name': 'Germaine Osborn',
+        'type': 'Tristique Aliquet PC',
+        'role': 'Lesotho',
+    },
+    {
+        'name': 'Nell Butler',
+        'type': 'Sit Amet Dapibus Industries',
+        'role': 'Cuba',
+    },
+    {
+        'name': 'Brent Stein',
+        'type': 'Eu Augue Porttitor LLP',
+        'role': 'Cyprus',
+    },
+    {
+        'name': 'Alexandra Shaw',
+        'type': 'Aenean Gravida Limited',
+        'role': 'Uruguay',
+    },
+    {
+        'name': 'Veronica Allison',
+        'type': 'Aliquet Diam Sed Institute',
+        'role': 'Samoa',
+    },
+    {
+        'name': 'Katelyn Gamble',
+        'type': 'Sed Associates',
+        'role': 'Mauritius',
+    },
+    {
+        'name': 'James Greer',
+        'type': 'A Dui Incorporated',
+        'role': 'Norway',
+    },
+    {
+        'name': 'Cain Vasquez',
+        'type': 'Nulla Facilisis Suspendisse Institute',
+        'role': 'China',
+    },
+    {
+        'name': 'Shaeleigh Barr',
+        'type': 'Eleifend Cras Institute',
+        'role': 'Ghana',
+    },
+    {
+        'name': 'Baker Mckay',
+        'type': 'Ut Sagittis Associates',
+        'role': 'Isle of Man',
+    },
+    {
+        'name': 'Jayme Pace',
+        'type': 'Cras Eu Tellus Associates',
+        'role': 'Bouvet Island',
+    },
+    {
+        'name': 'Reuben Albert',
+        'type': 'Lobortis Institute',
+        'role': 'Zambia',
+    },
+    {
+        'name': 'Idola Burns',
+        'type': 'Non Industries',
+        'role': 'Myanmar',
+    },
+    {
+        'name': 'Laura Macias',
+        'type': 'Phasellus Inc.',
+        'role': 'Mauritania',
+    },
+    {
+        'name': 'Nichole Salas',
+        'type': 'Duis PC',
+        'role': 'Madagascar',
+    },
+    {
+        'name': 'Hunter Walter',
+        'type': 'Ullamcorper Duis Cursus Foundation',
+        'role': 'Brazil',
+    },
+    {
+        'name': 'Asher Rich',
+        'type': 'Mauris Ipsum LLP',
+        'role': 'Paraguay',
+    },
+    {
+        'name': 'Angela Carlson',
+        'type': 'Donec Tempor Institute',
+        'role': 'Papua New Guinea',
+    },
+    {
+        'name': 'James Dorsey',
+        'type': 'Ipsum Leo Associates',
+        'role': 'Congo (Brazzaville)',
+    },
+    {
+        'name': 'Wesley Cobb',
+        'type': 'Nunc Est Incorporated',
+        'role': 'Australia',
+    },
+    {
+        'name': 'Meghan Stephens',
+        'type': 'Interdum PC',
+        'role': 'Turkey',
+    },
+    {
+        'name': 'Bertha Herrera',
+        'type': 'Amet Limited',
+        'role': 'Kenya',
+    },
+    {
+        'name': 'Karina Key',
+        'type': 'Quisque Varius Nam Company',
+        'role': 'France',
+    },
+    {
+        'name': 'Uriel Carson',
+        'type': 'Penatibus PC',
+        'role': 'Venezuela',
+    },
+    {
+        'name': 'Mira Baird',
+        'type': 'Felis Orci PC',
+        'role': 'Niue',
+    },
+    {
+        'name': 'Ursula Parrish',
+        'type': 'Ac Corporation',
+        'role': 'Macao',
+    },
+    {
+        'name': 'Josephine Sykes',
+        'type': 'Blandit Congue Limited',
+        'role': 'Holy See (Vatican City State)',
+    },
+    {
+        'name': 'Maggie Sims',
+        'type': 'Vulputate Posuere Industries',
+        'role': 'Sudan',
+    },
+    {
+        'name': 'Rogan Fuentes',
+        'type': 'Vestibulum Accumsan Neque Company',
+        'role': 'Jersey',
+    },
+    {
+        'name': 'Maya Haney',
+        'type': 'Ac Foundation',
+        'role': 'Falkland Islands',
+    },
+    {
+        'name': 'Aquila Battle',
+        'type': 'Sociis Natoque Penatibus Foundation',
+        'role': 'Azerbaijan',
+    },
+    {
+        'name': 'Connor Coleman',
+        'type': 'Orci Lacus Vestibulum Foundation',
+        'role': 'Croatia',
+    },
+    {
+        'name': 'Charity Thomas',
+        'type': 'Convallis Ligula Donec Inc.',
+        'role': 'Benin',
+    },
+    {
+        'name': 'Blythe Powers',
+        'type': 'Amet Orci Limited',
+        'role': 'Falkland Islands',
+    },
+    {
+        'name': 'Adria Battle',
+        'type': 'Ornare Lectus Incorporated',
+        'role': 'British Indian Ocean Territory',
+    },
+    {
+        'name': 'Melanie Mcintyre',
+        'type': 'Nunc Corp.',
+        'role': 'Mongolia',
+    },
+    {
+        'name': 'Keely Bauer',
+        'type': 'Nec Tempus Institute',
+        'role': 'Somalia',
+    },
+    {
+        'name': 'Noelani Strong',
+        'type': 'Nec LLP',
+        'role': 'Iran',
+    },
+    {
+        'name': 'Jeanette Henderson',
+        'type': 'Eu Elit Nulla Corporation',
+        'role': 'Italy',
+    },
+    {
+        'name': 'Candace Huber',
+        'type': 'Sed Institute',
+        'role': 'Uganda',
+    },
+    {
+        'name': 'Bethany Potter',
+        'type': 'Vivamus Nibh Dolor Incorporated',
+        'role': 'Puerto Rico',
+    },
+    {
+        'name': 'Whoopi Burks',
+        'type': 'Justo Inc.',
+        'role': 'Fiji',
+    },
+    {
+        'name': 'Sheila Long',
+        'type': 'Diam Associates',
+        'role': 'Sao Tome and Principe',
+    },
+    {
+        'name': 'Sonya Church',
+        'type': 'Laoreet Institute',
+        'role': 'Grenada',
+    },
+    {
+        'name': 'Shaine Forbes',
+        'type': 'Eu Arcu LLP',
+        'role': 'Cyprus',
+    },
+    {
+        'name': 'Alexandra Patrick',
+        'type': 'Ligula Donec Inc.',
+        'role': 'Viet Nam',
+    },
+    {
+        'name': 'Patience Vincent',
+        'type': 'Sem Molestie Associates',
+        'role': 'Philippines',
+    },
+    {
+        'name': 'Evelyn Smith',
+        'type': 'Fusce Industries',
+        'role': 'Togo',
+    },
+    {
+        'name': 'Kieran Gonzalez',
+        'type': 'Non Corp.',
+        'role': 'Equatorial Guinea',
+    },
+    {
+        'name': 'Molly Oneil',
+        'type': 'Non Dui Consulting',
+        'role': 'Belize',
+    },
+    {
+        'name': 'Nigel Davenport',
+        'type': 'Ullamcorper Velit In Industries',
+        'role': 'Vanuatu',
+    },
+    {
+        'name': 'Thor Young',
+        'type': 'Malesuada Consulting',
+        'role': 'French Southern Territories',
+    },
+    {
+        'name': 'Finn Delacruz',
+        'type': 'Lorem Industries',
+        'role': 'Cocos (Keeling) Islands',
+    },
+    {
+        'name': 'Lane Henderson',
+        'type': 'Pede Foundation',
+        'role': 'Kazakhstan',
+    },
+    {
+        'name': 'Shea Potter',
+        'type': 'Curabitur Limited',
+        'role': 'Timor-Leste',
+    },
+    {
+        'name': 'Brynn Yang',
+        'type': 'Ut Limited',
+        'role': 'Mayotte',
+    },
+    {
+        'name': 'Kylan Fuentes',
+        'type': 'Sapien Aenean Associates',
+        'role': 'Brazil',
+    },
+    {
+        'name': 'Lionel Mcbride',
+        'type': 'Ipsum PC',
+        'role': 'Portugal',
+    },
+    {
+        'name': 'Paul Lucas',
+        'type': 'Eget LLP',
+        'role': 'Nicaragua',
+    },
+    {
+        'name': 'Lareina Williamson',
+        'type': 'Imperdiet Ullamcorper Ltd',
+        'role': 'Cocos (Keeling) Islands',
+    },
+    {
+        'name': 'Amy Acevedo',
+        'type': 'Id Institute',
+        'role': 'Cook Islands',
+    },
+    {
+        'name': 'Nomlanga Silva',
+        'type': 'Eget LLC',
+        'role': 'Belize',
+    },
+    {
+        'name': 'Amena Stone',
+        'type': 'Enim Incorporated',
+        'role': 'Guinea',
+    },
+    {
+        'name': 'Danielle Coffey',
+        'type': 'Feugiat Placerat Corp.',
+        'role': 'Sao Tome and Principe',
+    },
+    {
+        'name': 'Buffy Russell',
+        'type': 'Lacus Quisque Ltd',
+        'role': 'Ecuador',
+    },
+    {
+        'name': 'Kaitlin Lamb',
+        'type': 'Malesuada Fringilla Est Associates',
+        'role': 'Algeria',
+    },
+    {
+        'name': 'Leilani Yates',
+        'type': 'Mus Proin LLC',
+        'role': 'South Sudan',
+    },
+    {
+
+        'name': 'Jemima Moon',
+        'type': 'Phasellus Corp.',
+        'role': 'South Georgia and The South Sandwich Islands',
+    },
+    {
+        'name': 'Hiroko Schwartz',
+        'type': 'Neque Institute',
+        'role': 'Saint Vincent and The Grenadines',
+    },
+    {
+        'name': 'Nathaniel Jensen',
+        'type': 'Mi Tempor Limited',
+        'role': 'Dominica',
+    },
+    {
+        'name': 'Silas Sweeney',
+        'type': 'Ultrices Institute',
+        'role': 'Turkmenistan',
+    },
+    {
+        'name': 'Jermaine Barry',
+        'type': 'Dapibus Corporation',
+        'role': 'Uzbekistan',
+    },
+    {
+        'name': 'Tatiana Nichols',
+        'type': 'Nec Diam Industries',
+        'role': 'Cook Islands',
+    },
+    {
+        'name': 'Rama Waller',
+        'type': 'Sem Pellentesque LLC',
+        'role': 'Andorra',
+    }
+];
