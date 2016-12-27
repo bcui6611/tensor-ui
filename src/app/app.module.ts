8951fd92f63a923eb12ffa1a03207a3e74f7ec34
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-27 09:46:54 +0530

    Add following to the file  - User component  - Inventories table  - organization component  - teams component  - teams edit component  - team table component  - team user component  - team permissions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,23 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { InventoriesHomeComponent } from "./inventories/inventoriesHome/inventories-home.component";
+import { UsersComponent } from './settings/users/users.component'
 
-import {DashboardHomeModule } from "./dashboard/components/dashboardHome/dashboard-home.module";
-import {InventoriesTableModule } from './inventories/inventoriesTable/inventories-table.module';
+import { DashboardHomeModule } from "./dashboard/components/dashboardHome/dashboard-home.module";
+import { InventoriesTableModule } from './inventories/inventoriesTable/inventories-table.module';
 import { SettingsModule } from './settings/settings.module';
 import { SettingsComponent } from './settings/settings.component';
 import { SettingsHomeModule } from './settings/settingsHome/settings-home.module';
+import { UserAddComponent } from './settings/users/usersAdd/user-add.component';
+import { InventoriesTableComponent } from './settings/users/usersTable/users-table.component';
+import { OrganizationsComponent } from './settings/organizations/organizations.component';
+import { OrganizationsAddComponent } from './settings/organizations/organizationsAdd/organizations-add.component';
+import { ChunkPipe } from './shared/chunk.pipe';
+import { TeamsComponent } from './settings/teams/teams.component';
+import { TeamEditComponent } from './settings/teams/teamEdit/team-edit.component';
+import { TeamsTableComponent } from './settings/teams/teamsTable/teams-table.component';
+import { TeamUsersComponent } from './settings/teams/teamsUsers/team-users.component';
+import { TeamPermissionsComponent } from './settings/teams/teamPermissions/team-permissions.component'
 
 
 @NgModule({
@@ -44,8 +55,19 @@ import { SettingsHomeModule } from './settings/settingsHome/settings-home.module
         LoginComponent,
         DashboardComponent,
         InventoriesHomeComponent,
+        InventoriesTableComponent,
+        UsersComponent,
+        UserAddComponent,
+        OrganizationsComponent,
+        OrganizationsAddComponent,
+        ChunkPipe,
+        TeamsComponent,
+        TeamEditComponent,
+        TeamsTableComponent,
+        TeamUsersComponent,
+        TeamPermissionsComponent
     ],
-    providers: [appRoutingProviders, {provide: API_BASE, useValue: 'http://localhost:8010'}],
+    providers: [appRoutingProviders, {provide: API_BASE, useValue: 'http://10.76.196.18:8010'}],
     bootstrap: [AppComponent]
 })
 export class AppModule {
