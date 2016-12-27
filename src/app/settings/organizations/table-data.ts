d46a3b6ea6d85b3a3ad032deecaaa9e3709c61ca
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-27 09:48:25 +0530

    Add sample data for organization tables

---
+++ src/app/settings/organizations/table-data.ts
@@ -0,0 +1,22 @@
+export const OrganizationData: Array<any> = [
+    {
+        'id': "57bd426572adf36d9b10021e",
+        'name': 'Default',
+        'description': 'Integer Corporation',
+    },
+    {
+        'id': "57bd426d72adf36d9b10021f",
+        'name': 'Application Engineering',
+        'description': 'Application Engineering Department fgd gsdfgsdfgsfdgsfdgfsd sdf gdfg  sfgdsd',
+    },
+    {
+        'id': "57bd427372adf36d9b100220",
+        'name': 'Social',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a arcu lectus. Sed sem lorem, commodo mollis bibendum pellentesque, commodo sed odio. Maecenas non erat sapien. Praesent molestie eleifend lorem eget mattis. Praesent tincidunt felis id mauris eleifend, ut sodales massa fringilla. Maecenas aliquet ultrices elit facilisis commodo. Sed sit amet nunc cursus, condimentum orci quis, imperdiet magna. Vivamus semper luctus consectetur. Mauris dignissim, nulla eget vestibulum egestas, tortor risus semper magna, pulvinar venenatis magna mi euismod dolor.',
+    },
+    {
+        'id': "57bd427872adf36d9b100221",
+        'name': 'Apple',
+        'description': 'Integer Corporation',
+    },
+];
