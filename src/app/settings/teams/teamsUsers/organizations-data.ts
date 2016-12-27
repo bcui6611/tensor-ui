de938d6a4241b1d620ce7b201edee2a75dea390a
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-27 09:49:40 +0530

    Add organization sample data

---
+++ src/app/settings/teams/teamsUsers/organizations-data.ts
@@ -0,0 +1,404 @@
+export const OrganizationsTableData: Array<any> = [
+    {
+        'name': 'Victoria Cantrell',
+        'description': 'Integer Corporation'
+    },
+    {
+        'name': 'Pearl Crosby',
+        'description': 'In PC'
+    },
+    {
+        'name': 'Colette Foley',
+        'description': 'Lorem Inc.'
+    },
+    {
+        'name': 'Anastasia Shaffer',
+        'description': 'Dolor Nulla Semper LLC'
+    },
+    {
+        'name': 'Gabriel Castro',
+        'description': 'Sed Limited'
+    },
+    {
+        'name': 'Cherokee Ware',
+        'description': 'Tincidunt LLC'
+    },
+    {
+        'name': 'Barry Moss',
+        'description': 'Sociis Industries'
+    },
+    {
+        'name': 'Maryam Tucker',
+        'description': 'Elit Pede Malesuada Inc.'
+    },
+    {
+
+        'name': 'Constance Clayton',
+        'description': 'Auctor Velit Aliquam LLP'
+    },
+    {
+        'name': 'Rogan Tucker',
+        'description': 'Arcu Vestibulum Ante Associates'
+    },
+    {
+        'name': 'Emery Mcdowell',
+        'description': 'Gravida Company'
+    },
+    {
+        'name': 'Yael Greer',
+        'description': 'Orci Limited'
+    },
+    {
+        'name': 'Jared Burgess',
+        'description': 'Auctor Incorporated'
+    },
+    {
+        'name': 'Sharon Campbell',
+        'description': 'Elit Curabitur Sed Consulting'
+    },
+    {
+        'name': 'Yeo Church',
+        'description': 'Donec Vitae Erat PC'
+    },
+    {
+        'name': 'Kylie Barlow',
+        'description': 'Fermentum Risus Corporation'
+    },
+    {
+        'name': 'Nell Leonard',
+        'description': 'Vestibulum Consulting'
+    },
+    {
+        'name': 'Brandon Fleming',
+        'description': 'Donec Egestas Associates'
+    },
+    {
+        'name': 'Inga Pena',
+        'description': 'Et Magnis Dis Limited'
+    },
+    {
+        'name': 'Arden Russo',
+        'description': 'Est Tempor Bibendum Corp.'
+    },
+    {
+        'name': 'Liberty Gallegos',
+        'description': 'Nec Diam LLC'
+    },
+    {
+        'name': 'Dennis York',
+        'description': 'Nullam Suscipit Foundation'
+    },
+    {
+        'name': 'Petra Chandler',
+        'description': 'Pede Nonummy Inc.'
+    },
+    {
+        'name': 'Aurelia Marshall',
+        'description': 'Donec Consulting'
+    },
+    {
+        'name': 'Rose Carter',
+        'description': 'Enim Consequat Purus Industries'
+    },
+    {
+        'name': 'Denton Atkins',
+        'description': 'Non Vestibulum PC'
+    },
+    {
+        'name': 'Germaine Osborn',
+        'description': 'Tristique Aliquet PC'
+    },
+    {
+        'name': 'Nell Butler',
+        'description': 'Sit Amet Dapibus Industries'
+    },
+    {
+        'name': 'Brent Stein',
+        'description': 'Eu Augue Porttitor LLP'
+    },
+    {
+        'name': 'Alexandra Shaw',
+        'description': 'Aenean Gravida Limited'
+    },
+    {
+        'name': 'Veronica Allison',
+        'description': 'Aliquet Diam Sed Institute'
+    },
+    {
+        'name': 'Katelyn Gamble',
+        'description': 'Sed Associates'
+    },
+    {
+        'name': 'James Greer',
+        'description': 'A Dui Incorporated'
+    },
+    {
+        'name': 'Cain Vasquez',
+        'description': 'Nulla Facilisis Suspendisse Institute'
+    },
+    {
+        'name': 'Shaeleigh Barr',
+        'description': 'Eleifend Cras Institute'
+    },
+    {
+        'name': 'Baker Mckay',
+        'description': 'Ut Sagittis Associates'
+    },
+    {
+        'name': 'Jayme Pace',
+        'description': 'Cras Eu Tellus Associates'
+    },
+    {
+        'name': 'Reuben Albert',
+        'description': 'Lobortis Institute'
+    },
+    {
+        'name': 'Idola Burns',
+        'description': 'Non Industries'
+    },
+    {
+        'name': 'Laura Macias',
+        'description': 'Phasellus Inc.'
+    },
+    {
+        'name': 'Nichole Salas',
+        'description': 'Duis PC'
+    },
+    {
+        'name': 'Hunter Walter',
+        'description': 'Ullamcorper Duis Cursus Foundation'
+    },
+    {
+        'name': 'Asher Rich',
+        'description': 'Mauris Ipsum LLP'
+    },
+    {
+        'name': 'Angela Carlson',
+        'description': 'Donec Tempor Institute'
+    },
+    {
+        'name': 'James Dorsey',
+        'description': 'Ipsum Leo Associates'
+    },
+    {
+        'name': 'Wesley Cobb',
+        'description': 'Nunc Est Incorporated'
+    },
+    {
+        'name': 'Meghan Stephens',
+        'description': 'Interdum PC'
+    },
+    {
+        'name': 'Bertha Herrera',
+        'description': 'Amet Limited'
+    },
+    {
+        'name': 'Karina Key',
+        'description': 'Quisque Varius Nam Company'
+    },
+    {
+        'name': 'Uriel Carson',
+        'description': 'Penatibus PC'
+    },
+    {
+        'name': 'Mira Baird',
+        'description': 'Felis Orci PC'
+    },
+    {
+        'name': 'Ursula Parrish',
+        'description': 'Ac Corporation'
+    },
+    {
+        'name': 'Josephine Sykes',
+        'description': 'Blandit Congue Limited'
+    },
+    {
+        'name': 'Maggie Sims',
+        'description': 'Vulputate Posuere Industries'
+    },
+    {
+        'name': 'Rogan Fuentes',
+        'description': 'Vestibulum Accumsan Neque Company'
+    },
+    {
+        'name': 'Maya Haney',
+        'description': 'Ac Foundation'
+    },
+    {
+        'name': 'Aquila Battle',
+        'description': 'Sociis Natoque Penatibus Foundation'
+    },
+    {
+        'name': 'Connor Coleman',
+        'description': 'Orci Lacus Vestibulum Foundation'
+    },
+    {
+        'name': 'Charity Thomas',
+        'description': 'Convallis Ligula Donec Inc.'
+    },
+    {
+        'name': 'Blythe Powers',
+        'description': 'Amet Orci Limited'
+    },
+    {
+        'name': 'Adria Battle',
+        'description': 'Ornare Lectus Incorporated'
+    },
+    {
+        'name': 'Melanie Mcintyre',
+        'description': 'Nunc Corp.'
+    },
+    {
+        'name': 'Keely Bauer',
+        'description': 'Nec Tempus Institute'
+    },
+    {
+        'name': 'Noelani Strong',
+        'description': 'Nec LLP'
+    },
+    {
+        'name': 'Jeanette Henderson',
+        'description': 'Eu Elit Nulla Corporation'
+    },
+    {
+        'name': 'Candace Huber',
+        'description': 'Sed Institute'
+    },
+    {
+        'name': 'Bethany Potter',
+        'description': 'Vivamus Nibh Dolor Incorporated'
+    },
+    {
+        'name': 'Whoopi Burks',
+        'description': 'Justo Inc.'
+    },
+    {
+        'name': 'Sheila Long',
+        'description': 'Diam Associates'
+    },
+    {
+        'name': 'Sonya Church',
+        'description': 'Laoreet Institute'
+    },
+    {
+        'name': 'Shaine Forbes',
+        'description': 'Eu Arcu LLP'
+    },
+    {
+        'name': 'Alexandra Patrick',
+        'description': 'Ligula Donec Inc.'
+    },
+    {
+        'name': 'Patience Vincent',
+        'description': 'Sem Molestie Associates'
+    },
+    {
+        'name': 'Evelyn Smith',
+        'description': 'Fusce Industries'
+    },
+    {
+        'name': 'Kieran Gonzalez',
+        'description': 'Non Corp.'
+    },
+    {
+        'name': 'Molly Oneil',
+        'description': 'Non Dui Consulting'
+    },
+    {
+        'name': 'Nigel Davenport',
+        'description': 'Ullamcorper Velit In Industries'
+    },
+    {
+        'name': 'Thor Young',
+        'description': 'Malesuada Consulting'
+    },
+    {
+        'name': 'Finn Delacruz',
+        'description': 'Lorem Industries'
+    },
+    {
+        'name': 'Lane Henderson',
+        'description': 'Pede Foundation'
+    },
+    {
+        'name': 'Shea Potter',
+        'description': 'Curabitur Limited'
+    },
+    {
+        'name': 'Brynn Yang',
+        'description': 'Ut Limited'
+    },
+    {
+        'name': 'Kylan Fuentes',
+        'description': 'Sapien Aenean Associates'
+    },
+    {
+        'name': 'Lionel Mcbride',
+        'description': 'Ipsum PC'
+    },
+    {
+        'name': 'Paul Lucas',
+        'description': 'Eget LLP'
+    },
+    {
+        'name': 'Lareina Williamson',
+        'description': 'Imperdiet Ullamcorper Ltd'
+    },
+    {
+        'name': 'Amy Acevedo',
+        'description': 'Id Institute'
+    },
+    {
+        'name': 'Nomlanga Silva',
+        'description': 'Eget LLC'
+    },
+    {
+        'name': 'Amena Stone',
+        'description': 'Enim Incorporated'
+    },
+    {
+        'name': 'Danielle Coffey',
+        'description': 'Feugiat Placerat Corp.'
+    },
+    {
+        'name': 'Buffy Russell',
+        'description': 'Lacus Quisque Ltd'
+    },
+    {
+        'name': 'Kaitlin Lamb',
+        'description': 'Malesuada Fringilla Est Associates'
+    },
+    {
+        'name': 'Leilani Yates',
+        'description': 'Mus Proin LLC'
+    },
+    {
+
+        'name': 'Jemima Moon',
+        'description': 'Phasellus Corp.'
+    },
+    {
+        'name': 'Hiroko Schwartz',
+        'description': 'Neque Institute'
+    },
+    {
+        'name': 'Nathaniel Jensen',
+        'description': 'Mi Tempor Limited'
+    },
+    {
+        'name': 'Silas Sweeney',
+        'description': 'Ultrices Institute'
+    },
+    {
+        'name': 'Jermaine Barry',
+        'description': 'Dapibus Corporation'
+    },
+    {
+        'name': 'Tatiana Nichols',
+        'description': 'Nec Diam Industries'
+    },
+    {
+        'name': 'Rama Waller',
+        'description': 'Sem Pellentesque LLC',
+    }
+];
