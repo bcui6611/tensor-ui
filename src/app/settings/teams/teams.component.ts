668bd6aa49114f2818aeaf3c711bbb9c0cb54cd8
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2017-01-09 12:36:34 +0530

    Add support to view Team Add component when add clicked

--- src/app/settings/teams/teams.component.ts
+++ src/app/settings/teams/teams.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TeamEditComponent } from './teamEdit/team-edit.component';
 
 @Component({
     selector: 'teams-cmp',
     templateUrl: './teams.component.html'
 })
 export class TeamsComponent implements OnInit {
+    private routerSub: Subscription;
+    private path: Subscription;
+
+    isAdd: boolean;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private router: Router
+    ) {}
+
     ngOnInit(): void {
         console.log('hello `Teams` component');
+        this.routerSub = this._route.params.subscribe(params => {
+            
+        });
+        this.path = this._route.data.subscribe(data => { 
+            this.isAdd = data['addTeam']
+        })
     }
 }
--- src/app/settings/teams/teams.module.ts
+++ src/app/settings/teams/teams.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TeamsComponent } from './teams.component';
-import { TeamEditComponent } from './teamEdit/team-edit.component';
-import { TeamPermissionsComponent } from './teamPermissions/team-permissions.component';
-import { TeamsTableComponent } from './teamsTable/teams-table.component';
-import { TeamUsersComponent } from './teamsUsers/team-users.component';
+import { TeamEditComponent } from './teamEdit/team-edit.component'
 
 @NgModule({
     imports: [],
-    declarations: [
-        TeamsComponent,
-        TeamEditComponent,
-        TeamPermissionsComponent,
-        TeamsTableComponent,
-        TeamUsersComponent
-    ],
+    declarations: [TeamsComponent, TeamEditComponent],
     exports: [],
 })
-export class TeamsModule {}
+export class  TeamsModule {}
