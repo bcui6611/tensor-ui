fc6107d0fc299c95f65651a654b8c4471e7bec19
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2017-01-09 17:22:25 +0530

    Add credentials view

---
+++ src/app/settings/credentials/credentials.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CredentialsAddComponent } from './credentials-add/credentials-add.component'
+
+@Component({
+    selector: 'credentials-cmp',
+    templateUrl: './credentials.component.html',
+})
+export class CredentialsComponent implements OnInit {
+    private routerSub: Subscription;
+    private path: Subscription;
+    isAdd: boolean;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        console.log('hello `Credentials` Component');
+
+        this.path = this._route.data.subscribe(data => {
+            this.isAdd = data['addCredentials'];
+        })
+    }
+}
---
+++ src/app/settings/credentials/credentials.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CredentialsComponent } from './credentials.component';
+import { CredentialsAddComponent } from './credentials-add/credentials-add.component';
+
+@NgModule({
+    imports: [],
+    declarations: [CredentialsComponent, CredentialsAddComponent],
+    exports: [],
+})
+export class  CredentialsModule {}
