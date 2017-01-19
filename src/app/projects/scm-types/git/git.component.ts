44a9aa350b6c38c873f2ccc5b49684facea841cd
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2017-01-19 09:33:23 +0530

    Add Git component and view

---
+++ src/app/projects/scm-types/git/git.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'git-cmp',
+    templateUrl: './git.component.html'
+})
+export class GitComponent {
+    // On Submit or On select trigger set SCM type variable
+    // Based on that value, different components should be loaded
+}
