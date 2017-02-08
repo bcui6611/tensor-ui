daef779d57fcbec9d97bc82d45c3e36b3276a07e
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2017-02-08 15:27:20 +0530

    Add support to get organization details from API

--- src/app/settings/credentials/credentials-add/credentials-add.component.ts
+++ src/app/settings/credentials/credentials-add/credentials-add.component.ts
@@ -1,29 +1,59 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-    'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-    'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-    'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-    'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-    'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+import { CredentialsService } from '../../../services/credentials.service';
+import { OrganizationService } from '../../../services/organization.service';
+import { Organization } from '../../../models/organization';
+import { Credentials } from '../../../models/credentials';
+import { CredentialsType } from '../../../models/credentials-type'
 
 @Component({
     selector: 'credentials-add',
-    templateUrl: './credentials-add.component.html'
+    templateUrl: './credentials-add.component.html',
+    providers: [CredentialsService, OrganizationService]
 })
 export class CredentialsAddComponent implements OnInit {
     model: any
+    organizations: Organization[];
+    organizationList: string[];
+    selectedType = "n";
+
+    constructor (
+        private credentialsService: CredentialsService,
+        private organizationService: OrganizationService
+    ) {}
 
     search = (text$: Observable<string>) =>
         text$
             .debounceTime(200)
             .distinctUntilChanged()
             .map(term => term.length < 2 ? []
-                : states.filter(v => new RegExp(term, 'gi').test(v)).splice(0, 10));
+                : this.organizationList.filter(v => new RegExp(term, 'gi').test(v)).splice(0, 10));
+    
     ngOnInit(): void {
         console.log('hello `CredentialsAdd` component');
+        this.getOrganizations()
+    }
+
+    getOrganizations(): void {
+        this.organizationService.getOrganizations()
+        .subscribe(res => {
+            this.organizationList = [];
+            this.organizations = res
+            for(let organization of this.organizations) {
+                this.organizationList.push(organization.name);
+            }
+        },
+        err => {
+            console.log(err);
+        });
+    }
+
+    onChange(credentialType) {
+        console.log(credentialType);
+        this.selectedType = credentialType;
+    }
+
+    onNotify(message: CredentialsType): void {
+        console.log(message);
     }
 }
